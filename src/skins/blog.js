a4ddb008e71e1113d8c273a79e1bf35c65586f33
arkoly <1291636059@qq.com>
2019-12-19 18:19:44 +0800

    add jsData of blog page

---
+++ src/skins/blog.js
@@ -0,0 +1,72 @@
+const tools=[
+    {
+        title:"CSS工具推荐",
+        list:[
+            {url:'https://tool.css-js.com/rgba.html',title:'RGB转16进制色互转，16进制转rgb' },
+            {url:'https://www.cssmatic.com/box-shadow',title:'模拟 阴影样式 Box Shadow CSS Generator | CSSmatic' },
+            {url:'https://icons8.com/icons/new/',title:'免费在线或下载小图标 New Icons - Free Download PNG and SVG | Icons8' },
+            {url:'https://iconsvg.xyz/',title:'一套可以定制的 SVG 图标。ICONSVG - Quick customizable SVG icons for your project' },
+            {url:'https://www.iconfont.cn/collections/index?spm=a313x.7781069.1998910419.3',title:'Iconfont-阿里巴巴矢量图标库' },
+            {url:'https://www.iconfu.com/main#/icons/5/1//03b8e5-0-0',title:'一个可以在线定制、修改图标的网站。Iconfu - Customizable Icons' },
+            {url:'http://www.ruanyifeng.com/blog/2019/03/coloring-scheme.html',title:'普通人的网页配色方案 - 阮一峰的网络日志' },
+            {url:'http://blog.sina.com.cn/s/blog_5c3b139d0101deia.html',title:'【色卡】常用色谱简析，中国传统颜色卡，代码附RBG，HC_穿膛_新浪博客' },
+            {url:'https://colors.ichuantong.cn/',title:'桃红 - 中国传统颜色手册 Chinese Color Cheatsheet' },
+            {url:'https://css-tricks.com/almanac/',title:'一个国外的优秀前端开发博客，主要分享使用CSS样式的技巧、经验和教程等。值得前端开发者阅读收藏的国外网站' },
+            {url:'',title:'' },
+            {url:'',title:'' },
+        
+        ]
+    },
+    {
+        title:'其他工具推荐',
+        list:[
+            {url:'https://astexplorer.net/',title:'解析AST，可以直接查看前端代码的树结构' },
+            {url:'https://www.p2hp.com/screenrecord.html',title:'在线屏幕录制' },
+            {url:'http://mockjs.com/',title:'模拟后台返回的数据' },
+            {url:'https://faststone-capture.en.softonic.com/',title:'前端图片处理工具'},
+            {url:'http://asciimath.org/',title:'一个可以将数学公式由代码转换为符号的网站'},
+            {url:'https://differ.netlify.com/',title:'可以在线也可以下载   用于比较两个文件的不同 Compare two files easily' },
+            {url:'',title:'' },
+
+        ]
+    }
+]
+const learn=[
+    {
+        title:"学习网站推荐",
+        list:[
+            {url:'https://wangdoc.com/javascript/',title:'JavaScript 教程 - 网道'},
+            {url:'http://es6.ruanyifeng.com/#docs/intro',title:'ECMAScript 6 简介 - ECMAScript 6入门'},
+            {url:'https://jspang.com/',title:'技术胖——前端'},
+            {url:'',title:''},
+            {url:'',title:''},
+            {url:'',title:''},
+        ]
+    },
+    {
+        title:"项目推荐",
+        list:[
+            {url:'https://codepen.io/ivorjetski/pen/xMJoYO',title:'纯css实现的图片 Pure CSS Still Life - Water and Lemons' },
+            {url:'https://github.com/sampotts/plyr',title:'一个H5播放器插件' },
+            {url:'https://github.com/ruanyf/weekly/issues/795',title:'在线生成不一样的艺术二维码 ' },
+            {url:'http://kenwheeler.github.io/slick/',title:'轮播插件' },
+            {url:'https://github.com/cmiscm/leonsans',title:'实现文字的各种炫酷动画' },
+            {url:'https://ishadeed.com/article/unusual-use-cases-pseudo-elements/',title:'多种文本hover效果' },
+            {url:'https://mmacneil.github.io/devfolio/',title:'国外的一个简历模板网页'}
+        ]
+    },
+    {
+        title:"网站推荐",
+        list:[
+            {url:'http://www.ruanyifeng.com/blog/archives.html',title:'强烈推荐每周阅读院一峰老师的"科技爱好者周刊"'},
+            {url:'https://tophub.today/',title:'今日热榜，整理了各大平台的热点' },
+            {url:'https://websec.readthedocs.io/zh/latest/basic/http.html',title:'HTTP — Web安全学习笔记 1.0 文档' },
+            {url:'http://www.91ri.org/17757.html',title:'记录了网络安全方面的技术' },
+            {url:'https://www.manhuatai.com/',title:'漫画台，里面有各种漫画，推荐“哑舍”'},
+            {url:'https://unsplash.com/t/wallpapers',title:'国外的一个免费分享精美图片网站'},
+            {url:'',title:''},
+        ]
+    }
+]
+
+export{tools, learn}
